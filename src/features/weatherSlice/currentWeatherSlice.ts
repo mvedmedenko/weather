6bb70bd41b4a21bf821c6a1c8795fea0bf9e3f41
Maksym Vedmedenko <maksym.vedmedenko@gmail.com>
2024-01-29 13:51:13 -0600

    vite base

--- src/features/weatherSlice/currentWeatherSlice.ts
+++ src/features/weatherSlice/currentWeatherSlice.ts
@@ -3,10 +3,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../api/api';
+import { CurrentLocation, CurrentLocationWeather } from '../../types/types';
 
 type Weather = {
-  currentLocationWeather: any[];
-  currentLocation: any[]
+  currentLocationWeather: CurrentLocationWeather;
+  currentLocation: CurrentLocation
   forecast: any[]
   isLoading: boolean;
   isLoaded: boolean,
@@ -14,8 +15,47 @@ type Weather = {
 };
 
 const initialState: Weather = {
-  currentLocationWeather: [],
-  currentLocation: [],
+  currentLocationWeather: {
+    cloud: 0,
+    condition: {
+      code: 1000,
+      icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
+      text: "Sunny",
+    },
+    feelslike_c: 0,
+    feelslike_f: 0,
+    gust_kph: 0,
+    gust_mph: 0,
+    humidity: 0,
+    is_day: 1,
+    last_updated: '',
+    last_updated_epoch: 0,
+    precip_in: 0,
+    precip_mm: 0,
+    pressure_in: 0,
+    pressure_mb: 0,
+    temp_c: 0,
+    temp_f: 0,
+    uv: 0,
+    vis_km: 0,
+    vis_miles: 0,
+    wind_degree: 0,
+    wind_dir: '',
+    wind_kph: 0,
+    wind_mph: 0,
+  },
+
+  currentLocation: {
+    country: '',
+    lat: 0,
+    lon: 0,
+    localtime: '',
+    localtime_epoch: 0,
+    name: '',
+    region: '',
+    tz_id: ''
+  },
+
   forecast: [],
   isLoading: false,
   isLoaded: false,
